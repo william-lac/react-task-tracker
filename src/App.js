44bf8fc871f6b25a702b7ee7d2f4156044ddfb7b
william-lac <wlac217@gmail.com>
2021-10-26 21:33:17 -0700

    end of day commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Header'
+
+
+function App() {
+  const name = 'Willio';
+  const x = true;
+
+  return (
+    <div className='container'>
+      <Header />
+
+    </div>
+  );
+}
+
+
+export default App;
