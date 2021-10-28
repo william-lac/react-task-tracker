dd5bb52c2d9a3dd2185ffbcb465e5124a6e5df38
william-lac <wlac217@gmail.com>
2021-10-27 21:35:10 -0700

    added Task component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,30 +1,12 @@
+import Task from './Task'
 
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Grocery Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    }
-];
-
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
     return (
-        <div>
-
-        </div>
+        <>
+            {tasks.map( (task) => (
+                <Task key={task.id} task={task}/>
+            ))}
+        </>
     )
 }
 
